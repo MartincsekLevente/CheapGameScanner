1dae38d0f721e6f7a3ac10dc75295e358e5be2c5
Martincsek Levente <martincsekl@gmail.com>
2024-04-27 19:10:33 +0200

    now works for multiple inputs, also fix for the page not loading bug

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ const fs = require("fs");
 import { Scraper } from "./scraper";
 
 const scraper = new Scraper();
-scraper.main();
+const gameNames = ["Horizon Forbidden West", "Fallout 76", "Cyberpunk 2077", "Manor Lords", "Helldivers 2", "Dragons Dogma 2", "Elden Ring"];
+scraper.main(gameNames);
--- src/scraper.ts
+++ src/scraper.ts
@@ -5,18 +5,20 @@ export class Scraper {
     private readonly CHEAPEST_PRICE_SELECTOR = '#offers_table > div:nth-child(1) > div.offers-table-row-cell.buy-btn-cell > a.d-none.d-lg-block.buy-btn.x-offer-buy-btn.text-center > span'
     private readonly URL_PREFIX = 'https://www.allkeyshop.com/blog/buy-'
     private readonly URL_SUFFIX = '-cd-key-compare-prices/'
+    private readonly REQUEST_COOLDOWN_MS = 5000;
 
-    public async main() {
+    public async main(gameNames: string[]) {
         try {
-            const gameName = "Horizon Forbidden West";
-            const url = this.makeUrlFromName(gameName);
-            const html = await this.fetchPageWithHtml(url);
-            if (!html) {
-                return;
+            for (const gameName of gameNames) {
+                const url = this.makeUrlFromName(gameName);
+                const html = await this.fetchPageWithHtml(url);
+                if (!html) {
+                    console.error('Error fetching page HTML for', gameName);
+                    return;
+                }
+                const price = this.scrapePrices(html);
+                console.log(gameName + ":\nCheapest price:", price+"\n");
             }
-
-            const price = this.scrapePrices(html);
-            console.log(gameName + ":\nCheapest price:", price);
         } catch (error) {
             console.error('Error:', error);
         }
@@ -28,19 +30,32 @@ export class Scraper {
     }
 
     private async fetchPageWithHtml(url: string): Promise<string | null> {
-        try {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto(url, {waitUntil: 'domcontentloaded'});
-            await page.waitForSelector(this.CHEAPEST_PRICE_SELECTOR);
-            const html = await page.content();
-            await browser.close();
-            return html;
-        } catch (error) {
-            console.error('Error while fetching page HTML:', error);
-            return null;
+        let html: string | null = null;
+        let retries = 0;
+
+        while (retries < 5) {
+            try {
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                await page.goto(url, { waitUntil: 'domcontentloaded' });
+                await page.waitForSelector(this.CHEAPEST_PRICE_SELECTOR);
+                html = await page.content();
+                await browser.close();
+                break;
+            } catch (error) {
+                retries++;
+                //console.log(`Retrying (${retries} of 5)`);
+                if (retries >= 5) {
+                    console.error('Exceeded retry threshold. Unable to fetch page HTML.');
+                } else {
+                    await this.wait(this.REQUEST_COOLDOWN_MS);
+                }
+            }
         }
+
+        return html;
     }
+
 
     private scrapePrices(html: string): string {
         try {
@@ -50,6 +65,10 @@ export class Scraper {
             console.error('Error while scraping price from HTML:', error);
             return '';
         }
+    }
+
+    private wait(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
 
