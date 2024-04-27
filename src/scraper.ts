d7ba068db1e1d676e227485eb463b82129ff4e67
Martincsek Levente <martincsekl@gmail.com>
2024-04-27 18:00:11 +0200

    Refactor

--- src/scraper.ts
+++ src/scraper.ts
@@ -2,17 +2,19 @@ import cheerio = require("cheerio");
 import puppeteer from 'puppeteer';
 
 export class Scraper {
+    private readonly CHEAPEST_PRICE_SELECTOR = '#offers_table > div:nth-child(1) > div.offers-table-row-cell.buy-btn-cell > a.d-none.d-lg-block.buy-btn.x-offer-buy-btn.text-center > span'
+
     public async main() {
         try {
             const gameName = "Horizon Forbidden West";
             const url = this.makeUrlFromName(gameName);
-            const html = await this.fetchPageWithPuppeteer(url);
+            const html = await this.fetchPageWithHtml(url);
             if (!html) {
                 return;
             }
 
             const price = this.scrapePrices(html);
-            console.log(gameName+":" );
+            console.log(gameName + ":");
             console.log("Cheapest price:", price);
         } catch (error) {
             console.error('Error:', error);
@@ -24,20 +26,29 @@ export class Scraper {
         return `https://www.allkeyshop.com/blog/buy-${formattedGameName}-cd-key-compare-prices/`;
     }
 
-    async fetchPageWithPuppeteer(url: string) {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'domcontentloaded' });
-        await page.waitForSelector('#offers_table > div:nth-child(1) > div.offers-table-row-cell.buy-btn-cell > a.d-none.d-lg-block.buy-btn.x-offer-buy-btn.text-center > span');
-        const html = await page.content();
-        await browser.close();
-        return html;
+    private async fetchPageWithHtml(url: string): Promise<string | null> {
+        try {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(url, {waitUntil: 'domcontentloaded'});
+            await page.waitForSelector(this.CHEAPEST_PRICE_SELECTOR);
+            const html = await page.content();
+            await browser.close();
+            return html;
+        } catch (error) {
+            console.error('Error while fetching page HTML:', error);
+            return null;
+        }
     }
 
-    scrapePrices(html: string): string {
-        const $ = cheerio.load(html);
-        const price = $('#offers_table > div:nth-child(1) > div.offers-table-row-cell.buy-btn-cell > a.d-none.d-lg-block.buy-btn.x-offer-buy-btn.text-center > span').text().trim();
-        return price;
+    private scrapePrices(html: string): string {
+        try {
+            const $ = cheerio.load(html);
+            return $(this.CHEAPEST_PRICE_SELECTOR).text().trim();
+        } catch (error) {
+            console.error('Error while scraping price from HTML:', error);
+            return '';
+        }
     }
 }
 
